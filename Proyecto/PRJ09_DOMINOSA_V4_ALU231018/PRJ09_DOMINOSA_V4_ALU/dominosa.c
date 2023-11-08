9d125ea4b0f017cb640056e71e3f65eb946b31e2
Oscar Guasch <148442383+oguasch05@users.noreply.github.com>
2023-11-08 17:52:20 +0100

    Add files via upload

--- Proyecto/PRJ09_DOMINOSA_V4_ALU231018/PRJ09_DOMINOSA_V4_ALU/dominosa.c
+++ Proyecto/PRJ09_DOMINOSA_V4_ALU231018/PRJ09_DOMINOSA_V4_ALU/dominosa.c
@@ -3,20 +3,21 @@
 #include "fichero.h"
 
 #define SIZE_NOMBRE_FICHERO     80
-#define MAX_CASILLAS            11
+#define MAX_FILAS               11
+#define MAX_COLUMNAS            10
 
 typedef struct casilla
 {
         int valor; //valor casilla 0-9
         int e, s; //0 o 1 (true or false)  
-} tcasilla[121];
+} tcasilla;
 
 int main()
 {
-        tcasilla casilla;
+        tcasilla casilla[MAX_FILAS][MAX_COLUMNAS];
 
         unsigned int conexiones = 0, max_conexiones;
-        int err, f, c, n, nf, nc, num, t, x1, x2, i, num_casillas;
+        int err, f, c, n, nf, nc, num, t, x1, x2, num_casillas;
 	char nombre_fichero[SIZE_NOMBRE_FICHERO], charf = 'A', y1, y2;
 
 
@@ -34,18 +35,22 @@ int main()
 		nc = leer_int_fichero();
                 max_conexiones = nf*nc/2;
 
-                for(i=0;i<=nf*nc;i++)
+                for(f=0;f<nf;f++)
                 {
-                        casilla[i].valor = leer_int_fichero();
-                        casilla[i].e = 0;
-                        casilla[i].s = 0;
+                        for(c=0;c<nc;c++)
+                        {
+                                casilla[f][c].valor = leer_int_fichero();
+                                casilla[f][c].e = 0;
+                                casilla[f][c].s = 0;
+                        }
                 }
                 cerrar_fichero();
 	}
         while(conexiones < max_conexiones)
         {
+                conexiones = 0;
                 charf = 'A';
-                t = 0, i=0;
+                t = 0;
                 printf_color_negrita();
                 printf("\n");
 
@@ -66,33 +71,37 @@ int main()
                         for(c=0;c<nc;c++)
                         {
                                 num = leer_int_fichero();
-                                printf_color_num(casilla[i].valor);
-                                printf("%d", casilla[i].valor);
+                                printf_color_num(casilla[f][c].valor);
+                                printf("%d", casilla[f][c].valor);
                                 printf_reset_color();
                                 printf_color_negrita();
-                                if(casilla[i].e==1)
+                                if(casilla[f][c].e==1)
                                 {
-                                        printf("███");                                                
+                                        printf("███");
+                                        conexiones++;                                                
                                 }
                                 else
                                         printf("   ");
-                                i++;
                         } printf("\n");
-                        i = i-nc;
                         for(c=0;c<nc;c++)
                         {
                                 printf(" + ");
-                                if(casilla[i].s == 1)
-                                        printf("o");
+                                if(casilla[f][c].s == 1)
+                                {
+                                        printf("█");
+                                        conexiones++;
+                                }
                                 else
                                         printf(" ");
-                                i++;
                         } printf(" +\n");
 
                 }
 
                 printf("\n");
                 printf("\nConexiones: %u de %u\n\n", conexiones, max_conexiones);
+
+                if(conexiones == max_conexiones)
+                        break;
 
                 do
                 {               
@@ -117,14 +126,90 @@ int main()
                 }
                 while(x1==x2 && y1==y2 || x1!=x2 && y1!=y2 || x1-x2!=1 && y1-y2!=1 && x2-x1!=1 && y2-y1!=1 || y1>nf-1 || y2>nf-1 || x1>nc || x2>nc);
                 if(y1>y2)
-                        casilla[nc*y2+x1].s = 1;
-                else if(y1<y2)
-                        casilla[nc*y1+x1].s = 1;
-                else if(x1>x2)
-                        casilla[nc*y1+x2].e = 1;
-                else if(x1<x2)
-                        casilla[nc*y1+x1].e = 1;       
+                {
+                        if(casilla[y2][x1].s == 1)
+                        {
+                                casilla[y2][x1].s = 0;
+                        }
+                        else
+                        {
+                                casilla[y1][x1].e = 0;
+                                casilla[y1][x1].s = 0;
+                                casilla[y2][x2].e = 0;
+                                casilla[y2][x2].s = 0;
+                                casilla[y1][x1-1].e = 0;
+                                casilla[y1-1][x1].s = 0;
+                                casilla[y2][x2-1].e = 0;
+                                casilla[y2-1][x2].s = 0;
 
+                                casilla[y2][x1].s = 1;
+                                
+                        }
+
+                }
+                else if(y1<y2)
+                {
+                        if(casilla[y1][x1].s == 1)
+                        {
+                                casilla[y1][x1].s = 0;
+                        }
+                        else
+                        {
+                                casilla[y1][x1].e = 0;
+                                casilla[y1][x1].s = 0;
+                                casilla[y2][x2].e = 0;
+                                casilla[y2][x2].s = 0;
+                                casilla[y1][x1-1].e = 0;
+                                casilla[y1-1][x1].s = 0;
+                                casilla[y2][x2-1].e = 0;
+                                casilla[y2-1][x2].s = 0;
+
+                                casilla[y1][x1].s = 1;
+                                
+                        }
+                }
+                else if(x1>x2)
+                {
+                        if(casilla[y1][x2].e == 1)
+                        {
+                                casilla[y1][x2].e = 0;
+                        }
+                        else
+                        {
+                                casilla[y1][x1].e = 0;
+                                casilla[y1][x1].s = 0;
+                                casilla[y2][x2].e = 0;
+                                casilla[y2][x2].s = 0;
+                                casilla[y1][x1-1].e = 0;
+                                casilla[y1-1][x1].s = 0;
+                                casilla[y2][x2-1].e = 0;
+                                casilla[y2-1][x2].s = 0;
+
+                                casilla[y1][x2].e = 1;
+                                
+                        }
+                }
+                else if(x1<x2)
+                {
+                        if(casilla[y1][x1].e == 1)
+                        {
+                                casilla[y1][x1].e = 0;
+                        }
+                        else
+                        {
+                                casilla[y1][x1].e = 0;
+                                casilla[y1][x1].s = 0;
+                                casilla[y2][x2].e = 0;
+                                casilla[y2][x2].s = 0;
+                                casilla[y1][x1-1].e = 0;
+                                casilla[y1-1][x1].s = 0;
+                                casilla[y2][x2-1].e = 0;
+                                casilla[y2-1][x2].s = 0;
+                                
+                                casilla[y1][x1].e = 1;
+                        }
+                }       
 
         }
+        printf("FELICIDADES! HAS RESUELTO ESTE DOMINOSA! B-)\n\n");
 }
