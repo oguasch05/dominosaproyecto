a899ba44bcdbbb714da08267486caaec9d2b9eef
Oscar Guasch <148442383+oguasch05@users.noreply.github.com>
2023-11-16 16:41:58 +0100

    Add files via upload

--- Proyecto/PRJ09_DOMINOSA_V4_ALU231018/PRJ09_DOMINOSA_V4_ALU/dominosa.c
+++ Proyecto/PRJ09_DOMINOSA_V4_ALU231018/PRJ09_DOMINOSA_V4_ALU/dominosa.c
@@ -5,6 +5,8 @@
 #define SIZE_NOMBRE_FICHERO     80
 #define MAX_FILAS               11
 #define MAX_COLUMNAS            10
+#define TRUE                    1
+#define FALSE                   0
 
 typedef struct casilla
 {
@@ -30,33 +32,33 @@ int main()
 	} 
         else 
         {
-		n = leer_int_fichero();
-		nf = leer_int_fichero();
-		nc = leer_int_fichero();
-                max_conexiones = nf*nc/2;
+		n = leer_int_fichero();   //numero mas grande que aparece
+		nf = leer_int_fichero();  //numero de filas
+		nc = leer_int_fichero();  //numero de columnas
+                max_conexiones = nf*nc/2; //numero de conexiones maximas
 
                 for(f=0;f<nf;f++)
                 {
                         for(c=0;c<nc;c++)
                         {
-                                casilla[f][c].valor = leer_int_fichero();
-                                casilla[f][c].e = 0;
-                                casilla[f][c].s = 0;
+                                casilla[f][c].valor = leer_int_fichero(); //guarda los valores del tablero en la matriz casilla[][].valor
+                                casilla[f][c].e = FALSE; //borra todas las conexiones en e
+                                casilla[f][c].s = FALSE; //borra todas las conexiones en s
                         }
                 }
                 cerrar_fichero();
 	}
-        while(conexiones < max_conexiones)
+        while(conexiones < max_conexiones && ) //cuando deje de cumplirse, el juego acaba
         {
-                conexiones = 0;
-                charf = 'A';
-                t = 0;
+                conexiones = 0; //numero de conexiones
+                charf = 'A'; //cabecera de fila
+                t = 0; //cabecera de columna
                 printf_color_negrita();
                 printf("\n");
 
                 for(c=0;c<nc;c++)
                 {
-                        printf("   %d", t);
+                        printf("   %d", t); //escribe la cabecera de las columnas
                         t++;
                 } printf("\n ");
                 for(c=0;c<nc;c++)
@@ -71,142 +73,142 @@ int main()
                         for(c=0;c<nc;c++)
                         {
                                 num = leer_int_fichero();
-                                printf_color_num(casilla[f][c].valor);
-                                printf("%d", casilla[f][c].valor);
+                                printf_color_num(casilla[f][c].valor); //cambia el color al color del valor de la casilla
+                                printf("%d", casilla[f][c].valor);     //escribe las casillas
                                 printf_reset_color();
                                 printf_color_negrita();
-                                if(casilla[f][c].e==1)
+                                if(casilla[f][c].e==TRUE)
                                 {
-                                        printf("███");
+                                        printf("███");  //escribe la conexion en caso de que casilla[][].e sea cierto
                                         conexiones++;                                                
                                 }
                                 else
-                                        printf("   ");
+                                        printf("   ");  //escribe la conexion en caso de que casilla[][].e sea falso
                         } printf("\n");
                         for(c=0;c<nc;c++)
                         {
                                 printf(" + ");
-                                if(casilla[f][c].s == 1)
+                                if(casilla[f][c].s == TRUE)
                                 {
-                                        printf("█");
+                                        printf("█"); //escribe la conexion en caso de que casilla[][].s sea cierto
                                         conexiones++;
                                 }
                                 else
-                                        printf(" ");
+                                        printf(" "); //escribe la conexion en caso de que casilla[][].e sea falso
                         } printf(" +\n");
 
                 }
 
                 printf("\n");
-                printf("\nConexiones: %u de %u\n\n", conexiones, max_conexiones);
+                printf("\nConexiones: %u de %u\n\n", conexiones, max_conexiones); //escribe "__ conexiones de __"
 
                 if(conexiones == max_conexiones)
                         break;
 
                 do
                 {               
-                        printf("Casillas a conectar/desconectar (ej: [A0B0]): "); 
-                        scanf("%c%d%c%d%*c", &y1, &x1, &y2, &x2);
+                        printf("Casillas a conectar/desconectar (ej: [A0B0]): "); //pide valores de dos casillas
+                        scanf("%c%d%c%d%*c", &y1, &x1, &y2, &x2);       //guarda valores de dos casillas en x1, x2, y1, y2
                         if(y1>='a'&&y1<='z')
                         {
-                                y1 = y1-'a';
+                                y1 = y1-'a'; //si la letra introducida es minuscula, y1 pasa a valer un numero (a=0,b=1,c=2,...)
                         }
                         if(y1>='A'&&y1<='Z')
                         {
-                                y1 = y1-'A';
+                                y1 = y1-'A'; //si la letra introducida es mayuscula, y1 pasa a valer un numero (A=0,B=1,C=2,...)
                         }
                         if(y2>='a'&&y2<='z')
                         {
-                                y2 = y2-'a';
+                                y2 = y2-'a'; //si la letra introducida es minuscula, y2 pasa a valer un numero (a=0,b=1,c=2,...)
                         }
                         if(y2>='A'&&y2<='Z')
                         {
-                                y2 = y2-'A';
+                                y2 = y2-'A'; //si la letra introducida es mayuscula, y2 pasa a valer un numero (A=0,B=1,C=2,...)
                         }
                 }
-                while(x1==x2 && y1==y2 || x1!=x2 && y1!=y2 || x1-x2!=1 && y1-y2!=1 && x2-x1!=1 && y2-y1!=1 || y1>nf-1 || y2>nf-1 || x1>nc || x2>nc);
+                while(x1==x2 && y1==y2 || x1!=x2 && y1!=y2 || x1-x2!=1 && y1-y2!=1 && x2-x1!=1 && y2-y1!=1 || y1>nf-1 || y2>nf-1 || x1>nc || x2>nc); //comprueba que la conexion es posible
                 if(y1>y2)
                 {
-                        if(casilla[y2][x1].s == 1)
+                        if(casilla[y2][x1].s == TRUE)
                         {
-                                casilla[y2][x1].s = 0;
+                                casilla[y2][x1].s = FALSE;
                         }
                         else
                         {
-                                casilla[y1][x1].e = 0;
-                                casilla[y1][x1].s = 0;
-                                casilla[y2][x2].e = 0;
-                                casilla[y2][x2].s = 0;
-                                casilla[y1][x1-1].e = 0;
-                                casilla[y1-1][x1].s = 0;
-                                casilla[y2][x2-1].e = 0;
-                                casilla[y2-1][x2].s = 0;
+                                casilla[y1][x1].e = FALSE;
+                                casilla[y1][x1].s = FALSE;
+                                casilla[y2][x2].e = FALSE;
+                                casilla[y2][x2].s = FALSE;
+                                casilla[y1][x1-1].e = FALSE;
+                                casilla[y1-1][x1].s = FALSE;
+                                casilla[y2][x2-1].e = FALSE;
+                                casilla[y2-1][x2].s = FALSE;
 
-                                casilla[y2][x1].s = 1;
+                                casilla[y2][x1].s = TRUE;
                                 
                         }
 
                 }
                 else if(y1<y2)
                 {
-                        if(casilla[y1][x1].s == 1)
+                        if(casilla[y1][x1].s == TRUE)
                         {
-                                casilla[y1][x1].s = 0;
+                                casilla[y1][x1].s = FALSE;
                         }
                         else
                         {
-                                casilla[y1][x1].e = 0;
-                                casilla[y1][x1].s = 0;
-                                casilla[y2][x2].e = 0;
-                                casilla[y2][x2].s = 0;
-                                casilla[y1][x1-1].e = 0;
-                                casilla[y1-1][x1].s = 0;
-                                casilla[y2][x2-1].e = 0;
-                                casilla[y2-1][x2].s = 0;
+                                casilla[y1][x1].e = FALSE;
+                                casilla[y1][x1].s = FALSE;
+                                casilla[y2][x2].e = FALSE;
+                                casilla[y2][x2].s = FALSE;
+                                casilla[y1][x1-1].e = FALSE;
+                                casilla[y1-1][x1].s = FALSE;
+                                casilla[y2][x2-1].e = FALSE;
+                                casilla[y2-1][x2].s = FALSE;
 
-                                casilla[y1][x1].s = 1;
+                                casilla[y1][x1].s = TRUE;
                                 
                         }
                 }
                 else if(x1>x2)
                 {
-                        if(casilla[y1][x2].e == 1)
+                        if(casilla[y1][x2].e == TRUE)
                         {
-                                casilla[y1][x2].e = 0;
+                                casilla[y1][x2].e = FALSE;
                         }
                         else
                         {
-                                casilla[y1][x1].e = 0;
-                                casilla[y1][x1].s = 0;
-                                casilla[y2][x2].e = 0;
-                                casilla[y2][x2].s = 0;
-                                casilla[y1][x1-1].e = 0;
-                                casilla[y1-1][x1].s = 0;
-                                casilla[y2][x2-1].e = 0;
-                                casilla[y2-1][x2].s = 0;
+                                casilla[y1][x1].e = FALSE;
+                                casilla[y1][x1].s = FALSE;
+                                casilla[y2][x2].e = FALSE;
+                                casilla[y2][x2].s = FALSE;
+                                casilla[y1][x1-1].e = FALSE;
+                                casilla[y1-1][x1].s = FALSE;
+                                casilla[y2][x2-1].e = FALSE;
+                                casilla[y2-1][x2].s = FALSE;
 
-                                casilla[y1][x2].e = 1;
+                                casilla[y1][x2].e = TRUE;
                                 
                         }
                 }
                 else if(x1<x2)
                 {
-                        if(casilla[y1][x1].e == 1)
+                        if(casilla[y1][x1].e == TRUE)
                         {
-                                casilla[y1][x1].e = 0;
+                                casilla[y1][x1].e = FALSE;
                         }
                         else
                         {
-                                casilla[y1][x1].e = 0;
-                                casilla[y1][x1].s = 0;
-                                casilla[y2][x2].e = 0;
-                                casilla[y2][x2].s = 0;
-                                casilla[y1][x1-1].e = 0;
-                                casilla[y1-1][x1].s = 0;
-                                casilla[y2][x2-1].e = 0;
-                                casilla[y2-1][x2].s = 0;
+                                casilla[y1][x1].e = FALSE;
+                                casilla[y1][x1].s = FALSE;
+                                casilla[y2][x2].e = FALSE;
+                                casilla[y2][x2].s = FALSE;
+                                casilla[y1][x1-1].e = FALSE;
+                                casilla[y1-1][x1].s = FALSE;
+                                casilla[y2][x2-1].e = FALSE;
+                                casilla[y2-1][x2].s = FALSE;
                                 
-                                casilla[y1][x1].e = 1;
+                                casilla[y1][x1].e = TRUE;
                         }
                 }       
 
