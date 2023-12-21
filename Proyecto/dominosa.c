49420959911e4334162e0db3b2fbac502c47cf4f
Oscar Guasch <148442383+oguasch05@users.noreply.github.com>
2023-12-21 12:58:53 +0000

    funciona en funciones, sucio

--- Proyecto/dominosa.c
+++ Proyecto/dominosa.c
@@ -29,8 +29,9 @@ typedef struct
 //--------------------------------------------------- DECLARACIÓN DE FUNCIONES --------------------------------------------------
 
 tablero inicializar(tablero t); //da a escoger un tablero de juego y lee el fichero que lo contiene guardando la información que este contiene y returnando el valor de ciertos paràmetros del tablero 
-int dibujar(tablero t); //printa el tablero escogido en la función inicializar y las conexiones que se van realizando
-int comprobar(tablero t); //comprueba que las conexiones introducidas sean válidas, posibles y correctas
+int dibujar(tablero t);         //printa el tablero escogido en la función inicializar y las conexiones que se van realizando
+void conectar(tablero *t);
+int comprobar(tablero t);       //comprueba que las conexiones introducidas sean válidas, posibles y correctas
 
 
 //---------------------------------------------------------- MAIN ---------------------------------------------------------------
@@ -60,122 +61,7 @@ int main()
                         conexiones_reales = comprobar(t);
                         if(conexiones_reales<max_conexiones)
                         {
-                        do
-                        {               
-                        printf("Casillas a conectar/desconectar (ej: [A0B0]): "); 			//pide valores de las dos casillas que se quieren conectar o desconectar
-                        scanf("%c%d%c%d%*c", &y1, &c1, &y2, &c2);       				//guarda los valores de las dos casillas a conectar en y1, y2, c1, c2 siendo 'y' la letra (char) de la fila escogida (que se pasará a int) y 'c' el número de la columna, 1 y 2 hacen referencia a la casilla primera o segunda que queremos conectar
-                        if(y1>='a'&&y1<='z')								//evalúa que la letra de la primera casilla sea minúscula 
-                        {
-                                f1 = y1-'a'; 								//si la letra introducida es minúscula, f1 pasa a valer un número (a=0,b=1,c=2,...)
-                        }
-                        if(y1>='A'&&y1<='Z')								//evalúa que la letra de la primera casilla sea mayúscula 
-                        {
-                                f1 = y1-'A'; 								//si la letra introducida es mayúscula, f1 pasa a valer un número (A=0,B=1,C=2,...)
-                        }
-                        if(y2>='a'&&y2<='z')								//evalúa que la letra de la segunda casilla sea minúscula 
-                        {
-                                f2 = y2-'a'; 								//si la letra introducida es minúscula, f2 pasa a valer un número (a=0,b=1,c=2,...)
-                        }
-                        if(y2>='A'&&y2<='Z')								//evalúa que la letra de la segunda casilla sea mayúscula
-                        {
-                                f2 = y2-'A'; 								//si la letra introducida es mayúscula, f2 pasa a valer un número (A=0,B=1,C=2,...)
-                        }
-                        }
-                        while(c1==c2 && f1==f2 || c1!=c2 && f1!=f2 || c1-c2!=1 && f1-f2!=1 && c2-c1!=1 && f2-f1!=1 || f1>t.nf-1 || f2>t.nf-1 || c1>t.nc || c2>t.nc); //comprueba que la conexión es posible mirando si las casillas escogidas son consecutivas
-                                        
-                        if(f1>f2) 									//mira que la conexión sea vertical ascendente (de abajo a arriba)
-                        {
-                        if(t.mat[f2][c1].s == TRUE)							//si la conexión evaluada ya existe, la borra
-                        {
-                                t.mat[f2][c1].s = FALSE;
-                        }
-                        else										//mira que la conexión no este hecha con anterioridad
-                        {
-                                t.mat[f1][c1].e = FALSE;						//mira que no haya conexiones que interfieran con la que queremos añadir y si las hay las elimina
-                                t.mat[f1][c1].s = FALSE;
-                                t.mat[f2][c2].e = FALSE;
-                                t.mat[f2][c2].s = FALSE;
-                                if(c1!=0)
-                                        t.mat[f1][c1-1].e = FALSE;
-                                if(c1!=0)
-                                        t.mat[f1-1][c1].s = FALSE;
-                                if(c2!=0)
-                                        t.mat[f2][c2-1].e = FALSE;
-                                if(f2!=0)
-                                        t.mat[f2-1][c2].s = FALSE;
-                                t.mat[f2][c1].s = TRUE;          
-                        }
-                        
-                        }
-                        else if(f1<f2) 									//mira que la conexión sea horizontal descendente (de arriba a abajo)
-                        {
-                        if(t.mat[f1][c1].s == TRUE)							//hace el mismo proceso que antes de evaluar si la conexión ya existía y hace los cambios necesarios para que se pueda hacer o deshacer la conexión
-                        {
-                                t.mat[f1][c1].s = FALSE;
-                        }
-                        else
-                        {
-                                t.mat[f1][c1].e = FALSE;
-                                t.mat[f1][c1].s = FALSE;
-                                t.mat[f2][c2].e = FALSE;
-                                t.mat[f2][c2].s = FALSE;
-                                if(c1!=0)
-                                        t.mat[f1][c1-1].e = FALSE;
-                                if(c1!=0)
-                                        t.mat[f1-1][c1].s = FALSE;
-                                if(c2!=0)
-                                        t.mat[f2][c2-1].e = FALSE;
-                                if(f2!=0)
-                                        t.mat[f2-1][c2].s = FALSE;
-                                t.mat[f1][c1].s = TRUE;                
-                        }
-                        }
-                        else if(c1>c2) 									//mismo procedimiento está vez evaluando las conexiones horizontales de derecha a izquierda
-                        {
-                        if(t.mat[f1][c2].e == TRUE)
-                        {
-                                t.mat[f1][c2].e = FALSE;
-                        }
-                        else
-                        {
-                                t.mat[f1][c1].e = FALSE;
-                                t.mat[f1][c1].s = FALSE;
-                                t.mat[f2][c2].e = FALSE;
-                                t.mat[f2][c2].s = FALSE;
-                                if(c1!=0)
-                                        t.mat[f1][c1-1].e = FALSE;
-                                if(c1!=0)
-                                        t.mat[f1-1][c1].s = FALSE;
-                                if(c2!=0)
-                                        t.mat[f2][c2-1].e = FALSE;
-                                if(f2!=0)
-                                        t.mat[f2-1][c2].s = FALSE;
-                                t.mat[f1][c2].e = TRUE;        
-                        }
-                        }
-                        else if(c1<c2)									//mismo procedimiento evaluando las conexiones horizontales de izquierda a derecha
-                        {
-                        if(t.mat[f1][c1].e == TRUE)
-                        {
-                                t.mat[f1][c1].e = FALSE;
-                        }
-                        else
-                        {
-                                t.mat[f1][c1].e = FALSE;
-                                t.mat[f1][c1].s = FALSE;
-                                t.mat[f2][c2].e = FALSE;
-                                t.mat[f2][c2].s = FALSE;
-                                if(c1!=0)
-                                        t.mat[f1][c1-1].e = FALSE;
-                                if(c1!=0)
-                                        t.mat[f1-1][c1].s = FALSE;
-                                if(c2!=0)
-                                        t.mat[f2][c2-1].e = FALSE;
-                                if(f2!=0)
-                                        t.mat[f2-1][c2].s = FALSE;      
-                                t.mat[f1][c1].e = TRUE;
-                        }
-                        }     
+                        conectar(&t);
                         }
                 }
                 while(conexiones_reales<max_conexiones);
@@ -218,7 +104,6 @@ tablero inicializar(tablero t)
 	}
 	return t; 									//devuelve una variable de struct tablero
 }
-
 
 int dibujar(tablero t)
 {
@@ -275,6 +160,129 @@ int dibujar(tablero t)
 	return conexiones;								//devuelve las conexiones realizadas
 }
 
+void conectar(tablero *t)
+{
+        int f, c, c1, c2, f1, f2;
+        char y1, y2;
+
+
+        do
+        {               
+                printf("Casillas a conectar/desconectar (ej: [A0B0]): "); 			//pide valores de las dos casillas que se quieren conectar o desconectar
+                scanf("%c%d%c%d%*c", &y1, &c1, &y2, &c2);       				//guarda los valores de las dos casillas a conectar en y1, y2, c1, c2 siendo 'y' la letra (char) de la fila escogida (que se pasará a int) y 'c' el número de la columna, 1 y 2 hacen referencia a la casilla primera o segunda que queremos conectar
+                if(y1>='a'&&y1<='z')								//evalúa que la letra de la primera casilla sea minúscula 
+                {
+                        f1 = y1-'a'; 								//si la letra introducida es minúscula, f1 pasa a valer un número (a=0,b=1,c=2,...)
+                }
+                if(y1>='A'&&y1<='Z')								//evalúa que la letra de la primera casilla sea mayúscula 
+                {
+                        f1 = y1-'A'; 								//si la letra introducida es mayúscula, f1 pasa a valer un número (A=0,B=1,C=2,...)
+                }
+                if(y2>='a'&&y2<='z')								//evalúa que la letra de la segunda casilla sea minúscula 
+                {
+                        f2 = y2-'a'; 								//si la letra introducida es minúscula, f2 pasa a valer un número (a=0,b=1,c=2,...)
+                }
+                if(y2>='A'&&y2<='Z')								//evalúa que la letra de la segunda casilla sea mayúscula
+                {
+                        f2 = y2-'A'; 								//si la letra introducida es mayúscula, f2 pasa a valer un número (A=0,B=1,C=2,...)
+                }
+        }
+        while(c1==c2 && f1==f2 || c1!=c2 && f1!=f2 || c1-c2!=1 && f1-f2!=1 && c2-c1!=1 && f2-f1!=1 || f1>t->nf-1 || f2>t->nf-1 || c1>t->nc || c2>t->nc); //comprueba que la conexión es posible mirando si las casillas escogidas son consecutivas
+                                        
+                        if(f1>f2) 									//mira que la conexión sea vertical ascendente (de abajo a arriba)
+                        {
+                        if(t->mat[f2][c1].s == TRUE)             					//si la conexión evaluada ya existe, la borra
+                        {
+                                t->mat[f2][c1].s = FALSE;
+                        }
+                        else										//mira que la conexión no este hecha con anterioridad
+                        {
+                                t->mat[f1][c1].e = FALSE;						//mira que no haya conexiones que interfieran con la que queremos añadir y si las hay las elimina
+                                t->mat[f1][c1].s = FALSE;
+                                t->mat[f2][c2].e = FALSE;
+                                t->mat[f2][c2].s = FALSE;
+                                if(c1!=0)
+                                        t->mat[f1][c1-1].e = FALSE;
+                                if(c1!=0)
+                                        t->mat[f1-1][c1].s = FALSE;
+                                if(c2!=0)
+                                        t->mat[f2][c2-1].e = FALSE;
+                                if(f2!=0)
+                                        t->mat[f2-1][c2].s = FALSE;
+                                t->mat[f2][c1].s = TRUE;          
+                        }
+                        
+                        }
+                        else if(f1<f2) 									//mira que la conexión sea horizontal descendente (de arriba a abajo)
+                        {
+                        if(t->mat[f1][c1].s == TRUE)							//hace el mismo proceso que antes de evaluar si la conexión ya existía y hace los cambios necesarios para que se pueda hacer o deshacer la conexión
+                        {
+                                t->mat[f1][c1].s = FALSE;
+                        }
+                        else
+                        {
+                                t->mat[f1][c1].e = FALSE;
+                                t->mat[f1][c1].s = FALSE;
+                                t->mat[f2][c2].e = FALSE;
+                                t->mat[f2][c2].s = FALSE;
+                                if(c1!=0)
+                                        t->mat[f1][c1-1].e = FALSE;
+                                if(c1!=0)
+                                        t->mat[f1-1][c1].s = FALSE;
+                                if(c2!=0)
+                                        t->mat[f2][c2-1].e = FALSE;
+                                if(f2!=0)
+                                        t->mat[f2-1][c2].s = FALSE;
+                                t->mat[f1][c1].s = TRUE;                
+                        }
+                        }
+                        else if(c1>c2) 									//mismo procedimiento está vez evaluando las conexiones horizontales de derecha a izquierda
+                        {
+                        if(t->mat[f1][c2].e == TRUE)
+                        {
+                                t->mat[f1][c2].e = FALSE;
+                        }
+                        else
+                        {
+                                t->mat[f1][c1].e = FALSE;
+                                t->mat[f1][c1].s = FALSE;
+                                t->mat[f2][c2].e = FALSE;
+                                t->mat[f2][c2].s = FALSE;
+                                if(c1!=0)
+                                        t->mat[f1][c1-1].e = FALSE;
+                                if(c1!=0)
+                                        t->mat[f1-1][c1].s = FALSE;
+                                if(c2!=0)
+                                        t->mat[f2][c2-1].e = FALSE;
+                                if(f2!=0)
+                                        t->mat[f2-1][c2].s = FALSE;
+                                t->mat[f1][c2].e = TRUE;        
+                        }
+                        }
+                        else if(c1<c2)					                                //mismo procedimiento evaluando las conexiones horizontales de izquierda a derecha
+                        {
+                        if(t->mat[f1][c1].e == TRUE)
+                        {
+                                t->mat[f1][c1].e = FALSE;
+                        }
+                        else
+                        {
+                                t->mat[f1][c1].e = FALSE;
+                                t->mat[f1][c1].s = FALSE;
+                                t->mat[f2][c2].e = FALSE;
+                                t->mat[f2][c2].s = FALSE;
+                                if(c1!=0)
+                                        t->mat[f1][c1-1].e = FALSE;
+                                if(c1!=0)
+                                        t->mat[f1-1][c1].s = FALSE;
+                                if(c2!=0)
+                                        t->mat[f2][c2-1].e = FALSE;
+                                if(f2!=0)
+                                        t->mat[f2-1][c2].s = FALSE;      
+                                t->mat[f1][c1].e = TRUE;
+                        }
+                        }     
+}
 
 int comprobar(tablero t)
 {
